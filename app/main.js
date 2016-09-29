9bea711b0f7659fefa4b98a0efb8e0f0baae450c
daniel-j <daniel.j.hede@gmail.com>
2016-09-29 22:17:47 +0200

    Add more options and customizability

--- app/main.js
+++ app/main.js
@@ -28,36 +28,60 @@ function createWindow () {
   if (p !== -1) {
     windowSize = args[p+1] || 'x'
   }
-  windowSize = windowSize.split('x').map((n) => parseInt(n, 10))
+
+  windowSize = windowSize.split('x')
+  windowSize.length = 2
+  windowSize = windowSize.map((n) => parseInt(n, 10) || 0)
 
   // Create the browser window.
   mainWindow = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: false, // very important,
-      plugins: true
+      nodeIntegration: false, // very important!
+      plugins: true,
+      allowDisplayingInsecureContent: true
     },
     title: 'Lutris Electron Runner',
     width: windowSize[0] || 800,
     height: windowSize[1] || 600,
     useContentSize: true,
-    autoHideMenuBar: true,
     center: true,
+    autoHideMenuBar: true,
     show: false,
     icon: icon,
-    fullscreen: args.includes('--fullscreen', 2),
-    backgroundColor: '#000000'
+    fullscreen: args.includes('--fullscreen', 2) && !args.includes('--disable-resizing'),
+    resizable: !args.includes('--disable-resizing'),
+    backgroundColor: '#000000',
+    frame: !args.includes('--frameless')
   })
 
   mainWindow.loadURL(inputUrl)
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  if (args.includes('--devtools')) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  if (args.includes('--disable-menu-bar')) {
+    mainWindow.setMenu(null)
+  }
+
+  let execjs = []
+
+  execjs.push(`
+    document.body.style.userSelect = "none"
+    document.body.style.webkitUserSelect = "none"
+  `)
+  if (args.includes('--disable-scrolling')) {
+    execjs.push('document.body.style.overflow = "hidden"')
+  }
+  if (args.includes('--hide-cursor')) {
+    execjs.push('document.body.style.cursor = "none"')
+  }
+
+  execjs = execjs.join(';\n')
 
   mainWindow.once('ready-to-show', () => {
-    if (args.includes('--disable-scrolling')) {
-      mainWindow.webContents.executeJavaScript('document.body.style.overflow = "hidden"')
-    }
     mainWindow.show()
+    mainWindow.webContents.executeJavaScript(execjs)
   })
 
   mainWindow.webContents.once('did-fail-load', (e, errorCode, errorDescription, validatedUrl, isMainFrame) => {
@@ -70,7 +94,7 @@ function createWindow () {
   mainWindow.webContents.on('new-window', handleRedirect)
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -79,12 +103,13 @@ function createWindow () {
 }
 
 function handleRedirect (e, url) {
-  if (url !== mainWindow.webContents.getURL()) {
+  if (url !== mainWindow.webContents.getURL() && !args.includes('--open-links')) {
     e.preventDefault()
     console.log('Opening url ' + url + ' in external browser')
     shell.openExternal(url)
   }
 }
+
 
 Menu.setApplicationMenu(appMenu)
 
@@ -94,7 +119,7 @@ Menu.setApplicationMenu(appMenu)
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -102,7 +127,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
