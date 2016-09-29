b1aacf75349d38cf662768f02ab0b75978b5de50
daniel-j <daniel.j.hede@gmail.com>
2016-09-29 12:05:27 +0200

    don't compress by default

--- build.js
+++ build.js
@@ -16,6 +16,7 @@ let options = {
 
 packager(options, (err, appPaths) => {
   if (err) throw err
+  if (process.argv[3] !== 'compress') return
   appPaths.forEach((path) => {
     console.log('Compressing ' + path + '.tar.gz')
     spawn('tar', ['-zcf', path + '.tar.gz', '-C', path, '.'], {stdio: ['ignore', process.stdout, process.stderr]})
